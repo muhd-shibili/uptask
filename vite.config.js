e784fe333bae80665546ee32d00d475bd8406998
muhd-shibili <cmshibili1610@gmail.com>
2026-07-29 20:36:58 +0400

    style-fix commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' 
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/uptask/', 
+  base: '/',        // ✅ change this from '/uptask/' to '/'
 })
